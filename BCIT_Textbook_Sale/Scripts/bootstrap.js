92a7dc8dbf766a9e15d9f5a25903130faaf7c45a
shanelboy <shanel_boy@hotmail.com>
2017-11-20 15:28:53 -0800

    menu

--- BCIT_Textbook_Sale/Scripts/bootstrap.js
+++ BCIT_Textbook_Sale/Scripts/bootstrap.js
@@ -1904,6 +1904,15 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ======================================================================== */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function () {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+    });
+    $('.side-nav .collapse').on("show.bs.collapse", function () {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");
+    });
+})
 
 
 +function ($) { "use strict";
